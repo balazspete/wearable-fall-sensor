1b80617711cfdd36dd0c14131788254632ede8c2
Balazs Pete <bazsip@gmail.com>
2014-03-09 14:14:35 +0000

    Rework serial message transmission

--- sensor/sensor_ble/sensor_ble.ino
+++ sensor/sensor_ble/sensor_ble.ino
@@ -72,47 +72,110 @@ void updateLoudness()
     }
 }
 
-void bleTransmitSensorData() {
-   if (Serial1.available()) {
-      Serial1.print("#MEASUREMENT|");
-      Serial1.print(Ax);
-      Serial1.print("|");
-      Serial1.print(Ay);
-      Serial1.print("|");
-      Serial1.print(Az);
-      Serial1.print("|");
-      Serial1.print(Gx);
-      Serial1.print("|");
-      Serial1.print(Gy);
-      Serial1.print("|");
-      Serial1.print(Gz);
-      Serial1.print("|");
-      Serial1.print(loudness);
-      Serial1.print("#");
+int dToBuffer(float value, char* buffer, int start)
+{
+  if (value >= 1000)
+  {
+    return -1;
+  }
+  
+  int mode = 0; 
+  long v = (long)(value * 1000LL);
+  
+  if (v < 0)
+  {
+    buffer[start++] = '-';
+    v = abs(v);
+  }
+  
+  int i = 6;
+  while (i > 0)
+  {
+    long b = pow(10, i--);
+    int _v = v/b;
+    if (mode || _v > 0)
+    { 
+      buffer[start++] = (char)(_v+48);
+      mode = 1;
+    }
+    v = v - _v*b;
+    if (i == 2)
+    {
+      buffer[start++] = '.';
+    }
+  }
+  
+  buffer[start] = '\0';
+  return start;
+}
+
+int copyOver(char origin[], char target[], unsigned int size, unsigned int start)
+{
+    int index = 0;
+    while (index < size)
+    {
+        target[start + index] = origin[index];
+        index++;
+    }
+    
+    return start+index;
+}
+
+void bleTransmitSensorData() 
+{
+
+  if (Serial1.available())
+  {
+    //if (Serial) {
+      char buffer [100];
+      int start = 0;
+      
+      char text[] = { '#', 'M', 'E', 'A', 'S', 'U', 'R', 'E', 'M', 'E', 'N', 'T', '|' };
+      
+      start = copyOver(text, buffer, 13, start);
+      start = dToBuffer(Ax, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(Ay, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(Az, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(Gx, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(Gy, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(Gz, buffer, start);
+      buffer[start++] = '|';
+      start = dToBuffer(loudness, buffer, start);
+      buffer[start++] = '#';
+      buffer[start] = '\0';
+      
+      Serial1.write(buffer);
       Serial1.flush();
-   }
+  }
 }
 
 void setup() {
-   // Console (remove when not used)
-   Serial.begin(9600);
+    // Console (remove when not used)
+    Serial.begin(9600);
     
-   // Connect to the BLE module
-   Serial1.begin(38400);
+    // Connect to the BLE module
+    Serial1.begin(38400);
    
-   Wire.begin();
+   
+    Wire.begin();
     
-   initialiseAccelerationAndGyro();
-   initialiseLoudnessSensor();
+    initialiseAccelerationAndGyro();
+    initialiseLoudnessSensor();
 }
 
 void loop() {
   while(true) {
-   updateAccelerationAndGyro();
-   updateLoudness();
-   bleTransmitSensorData();
+    updateAccelerationAndGyro();
+    updateLoudness();
+    //normalise()
+    bleTransmitSensorData();
    
-   delay(1000);
+    delay(500);
   }
 }
 
