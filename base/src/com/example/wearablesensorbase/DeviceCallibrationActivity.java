343e4a1cf582f93c7f2c72e07f99c2212927c783
Balazs Pete <bazsip@gmail.com>
2014-03-13 23:02:25 +0000

    Add callibration interface

---
+++ base/src/com/example/wearablesensorbase/DeviceCallibrationActivity.java
@@ -0,0 +1,50 @@
+package com.example.wearablesensorbase;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class DeviceCallibrationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_device_callibration);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.device_callibration, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ base/src/com/example/wearablesensorbase/callibration/Callibration.java
@@ -0,0 +1,31 @@
+package com.example.wearablesensorbase.callibration;
+
+import com.example.wearablesensorbase.data.SensorMeasurement;
+
+public class Callibration {
+
+	public void callibrateLeftwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+	public void callibrateRightwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+	public void callibrateForwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+	public void callibrateBackwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+	public void callibrateDownwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+	public void callibrateUpwards(SensorMeasurement first, SensorMeasurement second) {
+		
+	}
+	
+}
